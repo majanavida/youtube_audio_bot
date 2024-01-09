e571c1cb0b92cedb042b2e25e2c86e93522ed16e
Karushkin Gleb <onlythreebythree@gmail.com>
2024-01-09 19:24:04 +0400

    Delete useless module

--- services/download.py
+++
@@ -1,7 +0,0 @@
-from pytube import YouTube
-
-
-async def download_audio(url, message):
-    yt = YouTube(url)
-    stream = yt.streams.filter(only_audio=True).first()
-    stream.download(f'{message.chat.id}', f'{yt.title}')
